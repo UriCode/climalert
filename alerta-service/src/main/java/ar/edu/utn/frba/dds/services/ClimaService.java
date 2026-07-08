0cc2fff0983e7958d032f8b7f0b4e015de4aebba
Uriel Moreno <umoreno@frba.utn.edu.ar>
2026-07-08 02:37:03 +0000

    feat: implementacion del servicio coordinador

---
+++ alerta-service/src/main/java/ar/edu/utn/frba/dds/services/ClimaService.java
@@ -0,0 +1,50 @@
+package ar.edu.utn.frba.dds.services;
+
+import ar.edu.utn.frba.dds.domain.MedicionClimatica;
+import ar.edu.utn.frba.dds.domain.NotificadorAlerta;
+import ar.edu.utn.frba.dds.domain.ProveedorClima;
+import ar.edu.utn.frba.dds.domain.ReglaAlerta;
+import ar.edu.utn.frba.dds.domain.RepositorioMediciones;
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+public class ClimaService {
+    private final ProveedorClima proveedorClima;
+    private final RepositorioMediciones repositorioMediciones;
+    private final NotificadorAlerta notificadorAlerta;
+    private final ReglaAlerta reglaAlerta;
+
+    // Constructor (Inyección de dependencias)
+    public ClimaService(ProveedorClima proveedorClima, RepositorioMediciones repositorioMediciones, NotificadorAlerta notificadorAlerta, ReglaAlerta reglaAlerta) {
+        this.proveedorClima = proveedorClima;
+        this.repositorioMediciones = repositorioMediciones;
+        this.notificadorAlerta = notificadorAlerta;
+        this.reglaAlerta = reglaAlerta;
+    }
+
+    // Obtener y registrar el clima actual
+    public void registrarClimaActual(String ubicacion) {
+        MedicionClimatica medicion = proveedorClima.obtenerMedicion(ubicacion);
+        repositorioMediciones.guardar(medicion);
+    }
+
+    // Evaluar si el clima actual amerita una alerta
+    public void evaluarAlertas() {
+        Optional<MedicionClimatica> ultimaMedicionOpt = repositorioMediciones.obtenerUltima();
+        if (ultimaMedicionOpt.isEmpty()) {
+            System.out.println("No hay mediciones registradas para evaluar alertas.");
+            return;
+        }
+
+        MedicionClimatica ultimaMedicion = ultimaMedicionOpt.get();
+        if(reglaAlerta.esCritica(ultimaMedicion)) {
+            String mensaje = reglaAlerta.generarMensaje(ultimaMedicion);
+
+            Alerta alerta = Alerta.builder()
+            .fechaHora(LocalDateTime.now())
+            .mensaje(mensaje)
+            .medicionOriginal(ultimaMedicion)
+            .build();
+        }
+    }
+}
