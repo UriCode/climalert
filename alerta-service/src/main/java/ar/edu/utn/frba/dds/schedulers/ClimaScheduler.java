512955f369db8a41f67c4fed3fac325c01387944
Uriel Moreno <umoreno@frba.utn.edu.ar>
2026-07-09 19:23:32 +0000

    feat: implementacion de ClimaScheduler para la llamada periodica de obtener el clima actual y evaluar alertas

---
+++ alerta-service/src/main/java/ar/edu/utn/frba/dds/schedulers/ClimaScheduler.java
@@ -0,0 +1,28 @@
+package ar.edu.utn.frba.dds.schedulers;
+
+import ar.edu.utn.frba.dds.services.ClimaService;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ClimaScheduler {
+    private final ClimaService climaService;
+
+    public ClimaScheduler(ClimaService climaService) {
+        this.climaService = climaService;
+    }
+
+    // Cada 5 min obtiene el clima de CABA y lo guarda
+    @Scheduled(fixedRate = 300000)
+    public void obtenerClimaPeriodicamente(){
+        System.out.println("[Scheduler] Obteniedno clima actual para CABA...");
+        climaService.registrarClimaActual("CABA");
+    }
+
+    // Cada 1 min analiza la ultima medicion disponible
+    @Scheduled(fixedRate = 60000)
+    public void evaluarAlertasPeriodicamente() {
+        System.out.println("[Scheduler] Analizando última medicion disponible...");
+        climaService.evaluarAlertas();
+    }
+}
