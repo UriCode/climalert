204ec910e6cf611730a3f0c939097799bba94e56
Uriel Moreno <umoreno@frba.utn.edu.ar>
2026-07-09 16:04:09 +0000

    feat: implementacion de  MemoriaRepositorioMediciones para almacenamiento en memoria de la MedicionClimatica

---
+++ alerta-service/src/main/java/ar/edu/utn/frba/dds/services/repositories/MemoriaRepositorioMediciones.java
@@ -0,0 +1,25 @@
+package ar.edu.utn.frba.dds.services.repositories;
+
+import ar.edu.utn.frba.dds.domain.MedicionClimatica;
+import ar.edu.utn.frba.dds.domain.RepositorioMediciones;
+public class MemoriaRepositorioMediciones implements RepositorioMediciones {
+    private final List<MedicionClimatica> mediciones = new ArrayList<>();
+
+    @Override
+    public void guardar(MedicionClimatica medicion) {
+        mediciones.add(medicion);
+    }
+
+    @Override
+    public Optional<MedicionClimatica> obetenerUltima() {
+        if (mediciones.isEmpty()) {
+            return Optional.empty();
+        }
+        return Optional.of(mediciones.get(mediciones.size() - 1));
+    }
+
+    @Override
+    public List<MedicionClimatica> obtenerTodas() {
+        return new ArrayList<>(mediciones);
+    }
+}
