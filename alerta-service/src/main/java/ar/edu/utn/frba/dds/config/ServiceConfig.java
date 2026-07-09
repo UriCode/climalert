d4120fb3e6b42d74cd665e9f82d57915ea001425
Uriel Moreno <umoreno@frba.utn.edu.ar>
2026-07-09 19:15:07 +0000

    feat: agrego  ServiceConfig para la configuracion de  ClimaService y ReglaAlerta

---
+++ alerta-service/src/main/java/ar/edu/utn/frba/dds/config/ServiceConfig.java
@@ -0,0 +1,28 @@
+package ar.edu.utn.frba.dds.config;
+
+import ar.edu.utn.frba.dds.domain.NotificadorAlerta;
+import ar.edu.utn.frba.dds.domain.ProveedorClima;
+import ar.edu.utn.frba.dds.domain.ReglaAlerta;
+import ar.edu.utn.frba.dds.domain.ReglaAlertaCritica;
+import ar.edu.utn.frba.dds.domain.RepositorioMediciones;
+import ar.edu.utn.frba.dds.services.ClimaService;
+import org.springframework.contex.annotation.Bean;
+import org.springframework.contex.annotation.Configuration;
+
+@Configuration
+public class ServiceConfig {
+    @Bean
+    public ReglaAlerta reglaAlerta(){
+        return new ReglaAlertaCritica();
+    }
+
+    @Bean
+    public ClimaService climaService(
+        ProveedorClima proveedorClima;
+        RepositorioMediciones repositorioMediciones;
+        NotificadorAlerta notificadorAlerta;
+        ReglaAlerta reglaAlerta;
+    ) {
+        return new ClimaService(proveedorClima, repositorioMediciones, notificadorAlerta, reglaAlerta);
+    }
+}
