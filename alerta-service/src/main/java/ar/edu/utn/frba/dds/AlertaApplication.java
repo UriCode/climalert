a608998bd63648dcd6cb7ea7b5a9146866e1863b
Uriel Moreno <umoreno@frba.utn.edu.ar>
2026-07-09 19:28:06 +0000

    feat: agrego la clase principal AlertaApplication con la confifuracion de Spring Boot y el scheduling habilitado

---
+++ alerta-service/src/main/java/ar/edu/utn/frba/dds/AlertaApplication.java
@@ -0,0 +1,15 @@
+package ar.edu.utn.frba.dds;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication(scanBasePackages = {"ar.edu.frba.dds"})
+@EnableScheduling
+public class AlertaApplication {
+    public static void main (String[] args){
+        new SpringApplicationBuilder(AlertaApplication.class)
+            .profiles("dev")
+            .run(args);
+    }
+}
